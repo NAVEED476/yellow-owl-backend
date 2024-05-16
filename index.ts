a5dd306423ebd85ac3ce6a9ef927a2c505ea5548
Naveed Shaik <shaiknaveed435@gmail.com>
2024-05-16 13:27:32 +0530

    index.ts file updated

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
-
 import express, { Application, Request, Response } from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import studentRoutes from './routes/student.routes';
-const cors = require('cors');
 
 const app: Application = express();
 
@@ -19,10 +18,7 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
 
-
-
 const PORT = process.env.PORT || 7000;
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
